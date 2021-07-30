6940160994413001136157d8214d0e871880e784
Minh Nguyen <mail@minguyen.de>
2021-07-31 00:05:42 +0200

    fix

--- Kapitel 2/LogicalExpressions.java
+++ Kapitel 2/LogicalExpressions.java
@@ -16,8 +16,9 @@ public class LogicalExpressions {
         if (a > 1 || b > 1 || c > 1 || d > 1)
             result1 = true;
 
-        if ((a > 1 & b > 1 & c > 1) || (a > 1 & b > 1 & d > 1) || (a > 1 & c > 1 & d > 1) || (b > 1 & c > 1 & d > 1) || (b > 1 & d > 1) || (a > 1 & d > 1) ||
-                (c > 1 & d > 1) || (a > 1 & c > 1) || (c > 1 & b > 1) || a > 1 || b > 1 || c > 1 || d > 1)
+        if ((a > 1 & b > 1 & c > 1 & d <= 1) || (a > 1 & b > 1 & c <= 1 & d > 1) || (a > 1 & b <= 1 & c > 1 & d > 1) || (a <= 1 & b > 1 & c > 1 & d > 1) || (a <= 1 & b > 1 & c <= 1 & d > 1) || (a > 1 & b <= 1 & c <= 1 & d > 1) ||
+                (a <= 1 & b <= 1 & c > 1 & d > 1) || (a > 1 & b <= 1 & c > 1 & d <= 1) || (a <= 1 & c > 1 & b > 1 & d <= 1) || (a > 1 & b <= 1 & c <= 1 & d <= 1) || (a <= 1 & b > 1 & c <= 1 & d <= 1) ||(a <= 1 & b <= 1 & c > 1 & d <= 1) ||
+                    (a <= 1 & b <= 1 & c <= 1 & d > 1))
             result2 = true;
 
         if (a < 0 & b >= 0 & c >= 0 & d >= 0 || b < 0 & a >= 0 & c >= 0 & d >= 0 || c < 0 & a >= 0 & b >= 0 & d >= 0 | d < 0 & a >= 0 & b >= 0 & c >= 0)
@@ -25,7 +26,6 @@ public class LogicalExpressions {
 
         if (a > 0 & a > 10 || b > 0 & b > 10 || c > 0 & c > 10 || d > 0 & d > 10)
             result4 = true;
-        else if (a > 0 & a < 10 || b > 0 & b < 10 || c > 0 & c < 10 || d > 0 & d < 10) ;
 
         System.out.println(result1);
         System.out.println(result2);
